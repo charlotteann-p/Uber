d1a1566e9ccc3a43526a9456d2249ef0c66202e6
Charlotte Powning <capowning@gmail.com>
2019-03-23 13:33:43 -0400

    first commit

---
+++ Uber/Fare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+
+namespace Uber
+{
+    class Fare
+    {
+        public static double[] FareEstimator(int ride_time, int ride_distance, double[] cost_per_minute, double[] cost_per_mile)
+        {
+            double[] resultArray = new double[cost_per_minute.Length];
+
+            for (int i = 0; i < cost_per_minute.Length; i++)
+            {
+                double fare = (cost_per_minute[i] * ride_time) + (cost_per_mile[i] * ride_distance);
+                resultArray[i] = fare;
+            }
+            return resultArray;
+        }
+    }
+}
---
+++ Uber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+
+namespace Uber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Fare.FareEstimator(30, 7, new [] { 0.2, 0.35, 0.4, 0.45 }, new [] { 1.1, 1.8, 2.3, 3.5});
+        }
+    }
+}
